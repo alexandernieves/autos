3cadc1e8c128f5d42c3feef07379560c5f3694ec
Alexander Nieves <alexcodesolutionstm@gmail.com>
2024-11-24 12:54:01 -0400

    proyecto bien configurado con expo y cli

--- app.config.ts
+++ app.config.ts
@@ -1,47 +1,45 @@
 import 'dotenv/config';
 
 export default {
-  "expo": {
-    "name": "cabrera autos",
-    "slug": "Loquiereslotenemos",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+  expo: {
+    name: "cabrera autos",
+    slug: "Loquiereslotenemos",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
-    "updates": {
-      "fallbackToCacheTimeout": 0
+    updates: {
+      fallbackToCacheTimeout: 0,
     },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
-    "plugins": [
+    assetBundlePatterns: ["**/*"],
+    plugins: [
       [
         "expo-asset", // Agrega el plugin expo-asset para la gestión de imágenes
         {
-          "platforms": ["ios", "android", "web"]
-        }
-      ]
+          platforms: ["ios", "android", "web"],
+        },
+      ],
     ],
-    "ios": {
-      "supportsTablet": true,
-      "bundleIdentifier": "com.alexcode.org.cabrera"
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.alexcode.org.cabrera",
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#FFFFFF"
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#FFFFFF",
       },
-      "package": "com.alexcode.org.cabrera"
+      package: "com.alexcode.org.cabrera",
     },
-    "web": {
-      "favicon": "./assets/favicon.png"
+    web: {
+      favicon: "./assets/favicon.png",
     },
-    "scheme": "cabreraautos",
-    "extra": {
+    scheme: "cabreraautos",
+    extra: {
       apiKey: process.env.API_KEY,
       authDomain: process.env.AUTH_DOMAIN,
       projectId: process.env.PROJECT_ID,
@@ -49,8 +47,9 @@ export default {
       messagingSenderId: process.env.MESSAGING_SENDER_ID,
       appId: process.env.APP_ID,
       eas: {
-        projectId: "7f47da0d-53ef-4b77-88bd-0bdd2d66afd8"
-      }
-    }
-  }
-}
+        projectId: "7f47da0d-53ef-4b77-88bd-0bdd2d66afd8",
+      },
+    },
+    owner: "alexcode.org", // Agrega esta línea
+  },
+};
---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dependencies: {
+      'react-native': {
+        platforms: {
+          ios: null,  // Esto asegura que no se intente instalar dependencias de Android
+        },
+      },
+    },
+  };
+  
